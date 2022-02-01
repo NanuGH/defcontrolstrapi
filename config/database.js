d20e03d2209a2babab3088913c7cb0d4153211dc
NanuGH <66637776+NanuGH@users.noreply.github.com>
2022-02-01 10:56:29 -0100

    changin ssl status to true

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'd9n471bqphqrrp'),
       user: env('DATABASE_USERNAME', 'agzyvardhsowez'),
       password: env('DATABASE_PASSWORD', '79a31b1af9b0b897cf1a515f8430b14d36c2278b48435955bdb4cb18e104aed5'),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
